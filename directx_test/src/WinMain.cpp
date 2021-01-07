b3b5ebc35e7a060795191b96cb47f6732de1bf7e
orion <idbl64@outlook.com>
2021-01-07 12:51:43 -0500

    Window creation

--- directx_test/src/WinMain.cpp
+++ directx_test/src/WinMain.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <Windows.h>
 
-int main()
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+                    _In_ int nShowCmd)
 {
-	std::cout << "Hello DirectX\n";
+    const wchar_t class_name[] = L"class name";
+
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = DefWindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = class_name;
+    wc.hIconSm = nullptr;
+    RegisterClassEx(&wc);
+
+    HWND hwnd = CreateWindowEx(0, class_name, L"test", WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, 200, 200, 640, 480,
+                               nullptr, nullptr, hInstance, nullptr);
+
+    ShowWindow(hwnd, SW_SHOW);
+    while (true)
+        ;
+
+    return 0;
 }
